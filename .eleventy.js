57af33702a2c0e8f1a1dbbbc8c2f75d67a762a38
Saby22 <sabyasachi.tffs@gmail.com>
2021-01-07 23:50:06 +0530

    Updated package.json and minified HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,21 @@
+const htmlmin = require("html-minifier");
 module.exports = eleventyConfig => {
 
     eleventyConfig.addPassthroughCopy("./src/favicon");
+    eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+        if (outputPath.endsWith('.html')){
+            const minified = htmlmin.minify(content, {
+                collapseInlineTagWhitespace: false,
+                collapseWhitespace: true,
+                removeComments: true,
+                sortClassName: true,
+                useShortDoctype: true,
+            });
+            return minified;
+        }
+        return content;
+    });
+    
     return {
         addPassthroughCopy : true,
         dataTemplateEngine: "ejs",
